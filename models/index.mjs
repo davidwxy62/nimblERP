38241d4045c0cfb8ca552e3bea6d13ea6c73f7c9
davidwxy <davidwxy@umich.edu>
2023-05-16 22:21:38 -0400

    added User model

--- models/index.mjs
+++ models/index.mjs
@@ -1,3 +1,4 @@
+import User from "./user.mjs";
 import RoutingTable from "./routingTable.mjs";
 import Drawing from "./drawings.mjs";
 import Part from "./part.mjs";
@@ -7,6 +8,7 @@ import Vendor from "./vendor.mjs";
 import VendingOrder from "./vendingOrder.mjs";
 
 const models = {
+    User,
     RoutingTable,
     Drawing,
     Part,
---
+++ models/user.mjs
@@ -0,0 +1,41 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from "../config/database.mjs";
+
+class User extends Model {}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING(161),
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+    },
+    phone: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+    auth_level: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+}, {
+    sequelize,
+    modelName: 'User'
+});
+
+export default User;
